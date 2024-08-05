6d6c7c796739d0859ee195309c0a970518d298e0
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-08-05 13:00:00 -0400

    Update and rename hello.js to products.js

---
+++ netlify/functions/products.js
@@ -0,0 +1,12 @@
+const products = {
+    "U3VnYXJjYW5lIFN0aWNrcyAoNCBwYWNrKQ==": { name: "Sugarcane Sticks (4 pack)", img: "https://via.placeholder.com/200", price: 5.99, hot: true },
+    "NG96IFN1Z2FyY2FuZSBKdWljZQ==": { name: "Sugarcane Juice (4oz)", img: "https://via.placeholder.com/200", price: 8.99, hot: true },
+};
+
+exports.handler = async event => {
+    //const subject = event.queryStringParameters.name || 'World'
+    return {
+        statusCode: 200,
+        body: JSON.stringify(products),
+    }
+}
