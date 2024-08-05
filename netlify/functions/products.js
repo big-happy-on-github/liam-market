f5cf9f3af99da3c7ff8763bc72e9007a49d722f6
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-08-05 15:13:41 -0400

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,6 +1,7 @@
 const products = {
     "U3VnYXJjYW5lIFN0aWNrcyAoNCBwYWNrKQ==": { name: "Sugarcane Sticks (4 pack)", img: "https://via.placeholder.com/200", price: 5.99, hot: true },
     "NG96IFN1Z2FyY2FuZSBKdWljZQ==": { name: "Sugarcane Juice (4oz)", img: "https://via.placeholder.com/200", price: 8.99, hot: true },
+    "test": { name: "TEST", img: "https://via.placeholder.com/200", price: 100.00 },
 };
 
 exports.handler = async event => {
