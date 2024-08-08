464c4802e781d0796afdcbdc27e1c0f2b314f64d
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-08-08 10:23:16 -0400

    Update discounts.js

--- netlify/functions/discounts.js
+++ netlify/functions/discounts.js
@@ -1,1 +1,10 @@
+const discounts = {
+  "LIAMISTHEBESTMWUAHAHAHAHAHA": { percent: 0 }
+};
 
+exports.handler = async event => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(discounts),
+    }
+}
