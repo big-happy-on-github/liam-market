c7e3c9ee55985f64c0d9aac7d1ce754b60aa8aa3
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-08-15 10:13:32 -0400

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,5 +1,5 @@
 const products = {
-    "U3VnYXJjYW5lIFN0aWNrcyAoNCBwYWNrKQ==": { name: "Sugarcane Sticks (4 pack)", img: "https://via.placeholder.com/200", price: 5.99, hot: true, food: true },
+    "Lint-Lindor-Milk-Chocolate-Truffles-2-pack": { name: "Milk Chocolate (2 pack)", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: 0.99, hot: true, food: true },
     "NG96IFN1Z2FyY2FuZSBKdWljZQ==": { name: "Sugarcane Juice (4oz)", img: "https://via.placeholder.com/200", price: 8.99, hot: true , food: true },
     "test": { name: "TEST", img: "https://via.placeholder.com/200", price: 100.00 },
 };
