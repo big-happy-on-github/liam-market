7dcd8f5b4434677d81099b1269e90b1aeee7cfb8
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-08-19 15:25:00 -0400

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,7 +1,7 @@
 const products = {
     "Lint-Lindor-Milk-Chocolate-Truffles-2-pack": { name: "Milk Chocolate (2 pack)", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: 0.99, hot: true, food: true },
     "NG96IFN1Z2FyY2FuZSBKdWljZQ==": { name: "Sugarcane Juice (4oz)", img: "https://via.placeholder.com/200", price: 8.99, hot: true , food: true },
-    "test": { name: "TEST", img: "https://via.placeholder.com/200", price: 100.00 },
+    "test": { name: "TEST", img: "https://via.placeholder.com/200", price: 100.00, weight: 100, length: 1, width: 2, height: 3 },
 };
 
 exports.handler = async event => {
