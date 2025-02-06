b46e5a36aeee8ca553f99d4b43c2ea015d1eda3d
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2025-02-06 11:57:25 -0500

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,6 +1,6 @@
 const products = {
-    "math-hw": { name: "math hw", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: "chips", hot: true},
-    "english": { name: "english", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: "idk", hot: false}
+    "math-hw": { name: "math hw", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: "chips", description: "4.4 math hw, due 2/2", hot: true},
+    "english": { name: "english", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: "gummy candy", description: "idk"}
 };
 
 exports.handler = async event => {
