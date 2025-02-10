c91ca8ca578d8d46f4e93c8b253b7d02f7fb53da
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2025-02-10 14:50:19 -0500

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,5 +1,5 @@
 const products = {
-    "science-edpuzzle-2/10": { name: "science edpuzzle 2/10", img: "/answers/science edpuzzle 2.10/scienceedpuzzle2.10.png", price: "chips", description: "Mitosis vs. Meiosis Edpuzzle, due 2/11", hot: true},
+    "science-edpuzzle-2/10": { name: "science edpuzzle 2/10", img: "answers/science edpuzzle 2.10/scienceedpuzzle2.10.png", price: "chips", description: "Mitosis vs. Meiosis Edpuzzle, due 2/11", hot: true},
     "english": { name: "english", img: "https://pics.walgreens.com/prodimg/561575/100.jpg", price: "gummy candy", description: "idk"}
 };
 
